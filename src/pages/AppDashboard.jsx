3fd4a0d1c9b7b802b31f0df9a4819c9e417440e4
kajoljkambale <kajoljkambale@gmail.com>
2026-04-21 19:46:13 +0530

    Update AppDashboard.jsx

--- src/pages/AppDashboard.jsx
+++ src/pages/AppDashboard.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useCallback } from 'react'
 import { C, ADMIN_PWD, Ic, Spinner, Toast } from '../lib/ui.jsx'
 import { supabase as sb, loadAllData } from '../lib/supabase.js'
 
-import Login            from '../components/tabs/Login.jsx'
+import Login            from '../components/tabs/LoginTab.jsx'
 import Dashboard        from '../components/tabs/Dashboard.jsx'
 import EnrollmentTab    from '../components/tabs/EnrollmentTab.jsx'
 import StudentsTab      from '../components/tabs/StudentsTab.jsx'
